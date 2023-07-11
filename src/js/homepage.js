aa1dd8ff0af8f77fe212e642adf314d71b3d0816
jordankraude <jordankraudeTP@gmail.com>
2023-07-11 14:32:28 -0600

    pushing up homepage

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,28 +1,84 @@
+// gets list of all filter cards
+let cards = document.getElementsByClassName('card')
+console.log(cards)
 
 
-async function getOnePokemon(pokemonName){
+// changeActive function added
+function changeActive(){
 
-    dataDownload(url)
-    let card = document.createElement('div')
-    let pokemonName = 
-    let pokemonType = 
-    let pokemonHp =
-    let pokemonWeight = 
-    let pokemonHeight = 
-    let pokemonImage = 
+    // check to make sure function runs
+    console.log('changeActive running')
+
+    // finds the card which contains active and removes it
+    let active = document.getElementsByClassName('active')
+    active[0].classList.remove("active");
+
+    // adds active to clicked element
+    let clickedItem = this
+    clickedItem.classList.add('active');
+
+    let pokemonType = clickedItem.innerHTML
+    console.log(pokemonType)
+    if (pokemonType == "All"){
+        
+    }
+    else{
 
     }
+}
+
+
+let array = Array.from(cards)
+array.forEach(function(card){
+    card.addEventListener('click', changeActive, false);
+});
+
+
+// let cards = document.querySelectorAll('.card')
+
+// console.log(cards)
+// cards.forEach(el => el.addEventListener('click', event => {
+//     el.addEventListener('click', changeActive);
+//     console.log('finished')
+//   }));
+
+
+// async function getOnePokemon(pokemonName){
+
+//     dataDownload(url)
+//     let card = document.createElement('div')
+//     // let pokemonName = 
+//     // let pokemonType = 
+//     // let pokemonHp =
+//     // let pokemonWeight = 
+//     // let pokemonHeight = 
+//     // let pokemonImage = 
+
+//     }
 
 async function getAllPokemon(){
-    let card = document.createElement('div')
-    dataDownload(url)
-    let pokemonName = 
-    let pokemonType = 
+    let info = await fetch('pokebatleapi.onrender.com/pokemon/').then(response => response.json()).then(pokemon => {output(pokemon)})
+     // let pokemonName = 
+     // let pokemonType = 
+
+     for (var i = 0; i < 20; i++) {
+        let pokemonCard = document.createElement('div')
+        let pokemoninfo = pokemon.i
+        pokemoninfo.
+    }
+}
+
+function filter(pokemonType){
 
 }
 
 
 
-function dataDownload(url) {
-    fetch(url).then(response => response.json()).then(pokemon => {output(pokemon)});
-}
+// function dataDownload(url) {
+//     fetch(url).then(response => response.json()).then(pokemon => {output(pokemon)});
+// }
+
+
+
+
+
