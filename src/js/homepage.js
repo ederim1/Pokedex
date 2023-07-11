f89511fb575d176b32907cf994892c771f727e38
jordankraude <jordankraudeTP@gmail.com>
2023-07-11 16:05:44 -0600

    pushing up semi done homepage

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,13 +1,9 @@
 // gets list of all filter cards
 let cards = document.getElementsByClassName('card')
-console.log(cards)
 
 
 // changeActive function added
 function changeActive(){
-
-    // check to make sure function runs
-    console.log('changeActive running')
 
     // finds the card which contains active and removes it
     let active = document.getElementsByClassName('active')
@@ -19,7 +15,9 @@ function changeActive(){
 
     let pokemonType = clickedItem.innerHTML
     console.log(pokemonType)
-    if (pokemonType == "All"){
+    if (pokemonType === "All"){
+        document.querySelectorAll('.pokecard').style.display = "none";
+        console.log('runnning')
         
     }
     else{
@@ -57,14 +55,62 @@ array.forEach(function(card){
 //     }
 
 async function getAllPokemon(){
-    let info = await fetch('pokebatleapi.onrender.com/pokemon/').then(response => response.json()).then(pokemon => {output(pokemon)})
-     // let pokemonName = 
-     // let pokemonType = 
+    let info = await fetch('https://pokebattleapi.onrender.com/pokemon/').then(response => response.json()).then(pokemon => {output(pokemon)})
+    
+}
 
-     for (var i = 0; i < 20; i++) {
+function output(pokemon){
+    for (var i = 0; i < 16; i++) {
         let pokemonCard = document.createElement('div')
-        let pokemoninfo = pokemon.i
-        pokemoninfo.
+        let pokemoninfo = pokemon[i]
+        let pokemonName = document.createElement('h1')
+        pokemonName.innerHTML = pokemoninfo.name
+        pokemonCard.appendChild(pokemonName)
+        pokemonCard.className = "pokecard"
+        pokemonCard.classList.add(pokemoninfo.type[0])
+        pokemonCard.classList.add("card")
+
+        let pokemonImage = document.createElement('img')
+        pokemonImage.src = pokemoninfo.image
+        pokemonCard.appendChild(pokemonImage)
+
+        let pokemonWeight = document.createElement('h4')
+        pokemonWeight.innerHTML = "Weight: " + pokemoninfo.weight
+        pokemonCard.appendChild(pokemonWeight)
+
+        let pokemonHeight = document.createElement('h4')
+        pokemonHeight.innerHTML = "Height: "  + pokemoninfo.height
+        pokemonCard.appendChild(pokemonHeight)
+
+        let pokemonMovesContainer = document.createElement('ul')
+        pokemonMovesContainer.innerHTML = "Moves"
+        if (pokemoninfo.moves[0]){
+        let pokemonMove1 = document.createElement('li')
+        pokemonMove1.innerHTML = "1. " + pokemoninfo.moves[0].name 
+        pokemonMovesContainer.appendChild(pokemonMove1)
+        }
+        if (pokemoninfo.moves[1]){
+        let pokemonMove2 = document.createElement('li')
+        pokemonMove2.innerHTML = "2. " + pokemoninfo.moves[1].name
+        pokemonMovesContainer.appendChild(pokemonMove2)
+        }
+        if (pokemoninfo.moves[2]){
+        let pokemonMove3 = document.createElement('li')
+        pokemonMove3.innerHTML = pokemoninfo.moves[2]
+        pokemonMovesContainer.appendChild(pokemonMove3)
+        }
+        if (pokemoninfo.moves[3]){
+        let pokemonMove4 = document.createElement('li')
+        pokemonMove4.innerHTML = pokemoninfo.moves[3]
+        pokemonMovesContainer.appendChild(pokemonMove4)
+        }
+        pokemonCard.appendChild(pokemonMovesContainer)
+
+
+
+
+        
+        document.getElementById('pokemonDisplay').appendChild(pokemonCard)
     }
 }
 
@@ -72,7 +118,7 @@ function filter(pokemonType){
 
 }
 
-
+getAllPokemon()
 
 // function dataDownload(url) {
 //     fetch(url).then(response => response.json()).then(pokemon => {output(pokemon)});
